05d584c0bd99c67773f6a24519f7da9845ee0313
gabulsavul <gabriel.savio@clubpetro.com.br>
2018-05-17 14:20:29 -0300

    Initial commit

---
+++ android/app/src/main/java/br/com/clubpetro/savio/gabriel/notications/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.clubpetro.savio.gabriel.notications;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
